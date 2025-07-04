f10ebc598b4dc9e21cf57573a74e2e4fffa45db2
Igbo Kenneth <kcflexigbo@gmail.com>
2025-07-04 10:01:26 +0800

    infrastructure(backend): update gunicorn log file paths
    - Change accesslog and errorlog paths to /var/www/grad-app/grad-app/backend/logs/
    - This update ensures that log files are stored in the correct directory

--- backend/gunicorn_config.py
+++ backend/gunicorn_config.py
@@ -1,7 +1,7 @@
 bind = "unix:/var/www/grad-app/grad-app.sock"
 workers = 4
-accesslog = "/var/log/gunicorn/access.log"
-errorlog = "/var/log/gunicorn/error.log"
+accesslog = "/var/www/grad-app/grad-app/backend/logs/access.log"
+errorlog = "/var/www/grad-app/grad-app/backend/logs/error.log"
 loglevel = "info"
 k ="uvicorn.workers.UvicornWorker"
 capture_output = True
